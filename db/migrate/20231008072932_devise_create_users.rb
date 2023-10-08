6580482fe35869b00f97250b0fe355c05382702d
Shun-ST-TechCamp <namelesslnonamelunknown@gmail.com>
2023-10-08 18:13:13 +0900

    カラム名のうち間違えの修正

--- db/migrate/20231008072932_devise_create_users.rb
+++ db/migrate/20231008072932_devise_create_users.rb
@@ -6,9 +6,9 @@ class DeviseCreateUsers < ActiveRecord::Migration[7.0]
       t.string :email,              null: false, default: "", unique: true
       t.string :encrypted_password, null: false, default: ""
       t.string :last_name_kanji,    null: false
-      t.string :fast_name_kanji,    null: false
+      t.string :first_name_kanji,    null: false
       t.string :last_name_kana,     null: false
-      t.string :fast_name_kana,     null: false
+      t.string :first_name_kana,     null: false
       t.date   :birth_day,          null: false
       ## Recoverable
       t.string   :reset_password_token
