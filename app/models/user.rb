e2774c26437cb66db1dc269bc39bca90dd158bc0
Shun-ST-TechCamp <namelesslnonamelunknown@gmail.com>
2023-10-12 15:56:25 +0900

    エラーメッセージの表示

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,9 +6,9 @@ class User < ApplicationRecord
 
   validates :nickname,  presence:true
   validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i, message: 'Password is invalid. Include both letters and numbers' }
-  validates :last_name_kanji,  presence:true,   format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: 'Last name is invalid. Input full-width characters' }
-  validates :first_name_kanji,  presence:true,  format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: 'First name is invalid. Input full-width characters' }
-  validates :last_name_kana,  presence:true,    format: { with: /\A[ァ-ヶー－]+\z/, message: 'Last name is invalid. Input full-width katakana characters」' }
-  validates :first_name_kana,  presence:true,   format: { with: /\A[ァ-ヶー－]+\z/, message: 'First name is invalid. Input full-width katakana characters' }
+  validates :last_name_kanji,  presence:true,   format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: 'is invalid. Input full-width characters' }
+  validates :first_name_kanji,  presence:true,  format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: ' is invalid. Input full-width characters' }
+  validates :last_name_kana,  presence:true,    format: { with: /\A[ァ-ヶー－]+\z/, message: ' is invalid. Input full-width katakana characters」' }
+  validates :first_name_kana,  presence:true,   format: { with: /\A[ァ-ヶー－]+\z/, message: 'is invalid. Input full-width katakana characters' }
   validates :birth_day,  presence:true
 end
