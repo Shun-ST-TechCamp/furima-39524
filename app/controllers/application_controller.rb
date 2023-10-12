71741c4c167f7399e77b3eda0c607373bbeaca2d
Shun-ST-TechCamp <namelesslnonamelunknown@gmail.com>
2023-10-12 14:49:17 +0900

    ログインしている場合と、していない場合のビューファイルの表示の変更

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,6 +13,4 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :last_name_kanji, :first_name_kanji, :last_name_kana, :first_name_kana, :birth_day])
 
   end
-
-
 end
