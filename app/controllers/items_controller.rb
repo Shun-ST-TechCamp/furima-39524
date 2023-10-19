b042eb9c400ecfef356be44b6ab1c3a567ed751c
Shun-ST-TechCamp <namelesslnonamelunknown@gmail.com>
2023-10-19 16:54:41 +0900

    items_controllerの修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
   before_action :move_to_user_session, except: [:index]
   def index
-    @items = Item.all.order("created_at DESC")
+    @items = Item.order("created_at DESC")
   end
 
   def new
