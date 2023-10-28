ef10d189654bc15598458fb01a6b057e3cf287bf
Shun-ST-TechCamp <namelesslnonamelunknown@gmail.com>
2023-10-29 02:55:28 +0900

    売り切れになった時のビューファイル記述・コントローラーの修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,9 +1,10 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
   before_action :item_user, only: [:edit, :update, :destroy]
-  before_action :set_item, only: [:index, :show, :edit, :update, :destroy]
+  before_action :set_item, only: [:show, :edit, :update, :destroy]
   def index
-    @items = Item.order('created_at DESC')
+    @items = Item.includes(:buy).order('created_at DESC')
+ 
   end
 
   def new
