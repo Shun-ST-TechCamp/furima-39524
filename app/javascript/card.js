f84e6dfa524dac9246bff9d725c44d9f62cf2619
Shun-ST-TechCamp <namelesslnonamelunknown@gmail.com>
2023-10-30 14:07:13 +0900

    既にインスタンス化されていますエラーの修正

--- app/javascript/card.js
+++ app/javascript/card.js
@@ -1,10 +1,17 @@
+let isPayFunctionExecuted = false;
+
 const pay = () => {
+  if (isPayFunctionExecuted) {
+    return;
+  }
+  
   const publicKey = gon.public_key
   const payjp = Payjp(publicKey) 
   const elements = payjp.elements();  const numberElement = elements.create('cardNumber');
   const expiryElement = elements.create('cardExpiry');
   const cvcElement = elements.create('cardCvc');
-  
+  isPayFunctionExecuted = true;
+
   numberElement.mount('#number-form');
   expiryElement.mount('#expiry-form');
   cvcElement.mount('#cvc-form');
