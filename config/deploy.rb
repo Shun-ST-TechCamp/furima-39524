bf149805e27037be02625e15e4b8d728a9b7cf6a
Shun-ST-TechCamp <namelesslnonamelunknown@gmail.com>
2023-11-04 16:44:52 +0900

    自動デプロイ設定

---
+++ config/deploy.rb
@@ -0,0 +1,30 @@
+lock "~> 3.18.0"
+
+set :application, "furima-39524"
+set :repo_url, "git@github.com:Shun-ST-TechCamp/furima-39524.git"
+set :branch, 'main'
+
+# バージョンが変わっても共通で参照するディレクトリを指定
+set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
+
+set :rbenv_type, :user
+set :rbenv_ruby, '3.2.0' #カリキュラム通りに進めた場合、’3.2.0’ です
+
+# どの公開鍵を利用してデプロイするか
+set :ssh_options, auth_methods: ['publickey'],
+                                  keys: ['~/.ssh/TECK_CAMP_39524.pem'] 
+
+# プロセス番号を記載したファイルの場所
+set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
+
+# Unicornの設定ファイルの場所
+set :unicorn_config_path, -> { "#{current_path}/config/unicorn.rb" }
+set :keep_releases, 5
+
+# デプロイ処理が終わった後、Unicornを再起動するための記述
+after 'deploy:publishing', 'deploy:restart'
+namespace :deploy do
+  task :restart do
+    invoke 'unicorn:restart'
+  end
+end
