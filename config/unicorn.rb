e2b58913db9315837239159f5a5952872a532801
Shun-ST-TechCamp <namelesslnonamelunknown@gmail.com>
2023-11-04 16:09:38 +0900

    Unicornの設定

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,6 +1,6 @@
 #サーバ上でのアプリケーションコードが設置されているディレクトリを変数に入れておく
 app_path = File.expand_path('../../', __FILE__)
-
+listen "#{app_path}/tmp/sockets/unicorn.sock"
 #アプリケーションサーバの性能を決定する
 worker_processes 1
 
